9d71d2223acc8620fb8cb86a8f6298e12b6716f9
Vladislav Lovell <vladislav.prosvirkin.2000@mail.ru>
2022-09-21 14:08:02 +0300

    Добавил логгирование

--- vk_bot.py
+++ vk_bot.py
@@ -1,40 +1,46 @@
 # !/usr/bin/vk_env
 import random
+import logging
 import vk_api
-import vk_api.bot_longpoll
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from _token import token
 from _token import group_id
 """ В отдельном файле _token хранится значения token и group_id !
 """
-
+log = logging.getLogger('bot')
+stream_handler = logging.StreamHandler()
+log.addHandler(stream_handler)
+log.setLevel(logging.DEBUG)
+# logging.
 
 class Bot:
     def __init__(self, group_id, token):
         self.group_id = group_id
         self.token = token
         self.vk = vk_api.VkApi(token=token)
-        self.vk_long_poll = vk_api.bot_longpoll.VkBotLongPoll(self.vk, self.group_id)
+        self.vk_long_poll = VkBotLongPoll(self.vk, self.group_id)
 
         self.api_vk = self.vk.get_api()
 
     def run(self):
         for event in self.vk_long_poll.listen():
-            print('Произошло некое событие')
             #print(event) Позволяет лучше понять к чему обращаться
             try:
                 self.on_event(event)
             except Exception as exc:
-                print(exc)
+                log.exception('Ошибка в обработке события')
 
     def on_event(self, event):
-        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            log.info('Отправили это же сообщение обратно этому пользователю')
             self.api_vk.messages.send(
                 message=event.message.text,#message.text,
                 random_id=random.randint(0, 2 ** 20),
                 peer_id=event.message.peer_id,
             )
         else:
-            print("Неизвестное событие - ", event.type)
+            log.debug("Неизвестное событие - %s", event.type)
+            # print("Неизвестное событие - ", event.type)
 
 
 
